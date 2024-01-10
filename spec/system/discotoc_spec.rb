55236a34cefd5e6887cbb5be876f2fd604e12aaf
awesomerobot <kris.aubuchon@discourse.org>
2024-01-10 18:25:51 -0500

    add test for toggle

--- spec/system/discotoc_spec.rb
+++ spec/system/discotoc_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "DiscoTOC", system: true do
   fab!(:topic_1) { Fabricate(:topic) }
   fab!(:topic_2) { Fabricate(:topic, category: category, tags: [tag]) }
   
-
   fab!(:post_1) {
     Fabricate(:post, raw: "<div data-theme-toc='true'></div>\n\n# Heading 1\nContent for the first heading\n## Heading 2\nContent for the second heading\n### Heading 3\nContent for the third heading\n# Heading 4\nContent for the fourth heading", topic: topic_1)
   }
@@ -22,9 +21,18 @@ RSpec.describe "DiscoTOC", system: true do
     Fabricate(:post, raw: "intentionally \n long \n content \n so \n there's \n plenty \n to be \n scrolled \n past \n which \n will \n force \n the \n timeline \n to \n hide \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll \n scroll ", topic: topic_1)
   }
 
-
   it "table of contents appears when the relevant markup is added to first post in topic" do
     visit("/t/#{topic_1.id}")
+  end
+
+  it "clicking the toggle button toggles the timeline" do
+    visit("/t/#{topic_1.id}")
+
+    find(".timeline-toggle").click
+
+    expect(page).to have_css(".timeline-scrollarea-wrapper")
+
+    find(".timeline-toggle").click
 
     expect(page).to have_css(".d-toc-item.d-toc-h1")
   end
